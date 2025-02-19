2aa4ff63aad02aa5b346e088924de2a341e03d72
moein <moein191013895@gmail.com>
2025-02-19 18:51:25 +0330

    added new

--- moein_blog/settings.py
+++ moein_blog/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'moein_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'webl',
+        'USER':'postgres',
+        'PASSWORD':'moein',
+        'HOST':'localhost'
     }
 }
 
